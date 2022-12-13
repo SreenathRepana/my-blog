9bfb25763cb00ab8c660957b33be3a2cf51ff53a
Mogal Sulthan <mogal_sulthan@epam.com>
2022-12-13 18:36:06 +0530

    creating and linking articles list

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,7 +1,24 @@
-
+import { parsePath, useParams } from "react-router-dom";
+import articles from './article-content';
 
 export function ArticlePage () {
+    // const params = useParams();
+    // const articleId = params.articleId;
+
+    const {articleId} = useParams();
+    const article = articles.find( (articleItem) => {
+        return articleItem.name === articleId;
+    })
+ 
     return (
-        <h1> Article Page </h1>
+        <>
+            <h1> {article.title} </h1>
+            {article.content.map(paragraph => {
+                return (
+                    <p> {paragraph} </p>
+                );
+            })}
+        </>
+
     );
 }
--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,5 +1,19 @@
+import articles from './article-content';
+
 export function ArticlesListPage () {
     return (
-        <h1> Articles List Page </h1>
+        <>
+            <h1> Articles </h1>
+            {
+                articles.map(article => {
+                    return (
+                        <div>
+                            <h3> {article.title} </h3>
+                            <p> {article.content[0].substring(0,150)}... </p>
+                        </div>
+                    );
+                })
+            }
+        </>
     );
 }
